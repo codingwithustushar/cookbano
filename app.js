bc68f54dfc01a04d0656ab2517ba56999ba552f0
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2023-12-17 16:46:35 +0530

    hi

---
+++ app.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("inputbox")
+
+async function fetchdata(food){
+     food = input.value; 
+ try{
+    let data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${food}`);
+
+    let res = await data.json();
+    console.log(res);
+    
+ }
+ catch(error){
+    console.log(error)
+ }
+}
+// fetchdata();
+
+input.addEventListener("keypress",function(e){
+    if(e.key == "Enter"){
+        fetchdata();
+    }
+})
+
